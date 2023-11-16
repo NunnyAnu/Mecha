f6a03e496a298b405eaad021f08de70ad799ddea
bhambhambhambham <bhampr@gmail.com>
2023-11-16 19:53:25 +0700

    mgvhcghk

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from mqttsubscriber import MqttSubscriber
 from mqttpublisher import MqttPublisher
 import time
+from pypylon import pylon
+from pymongo import MongoClient
+import cv2
+from bson import Binary
+import numpy as np
 
 class Action:
     def __init__(self, broker, port, subscribing_topic, publishing_topic, username, password):
@@ -13,35 +18,81 @@ class Action:
         self.mqtt_subscriber = MqttSubscriber(self.broker, self.port, self.sub_topic)
         self.mqtt_publisher = MqttPublisher(self.broker, self.port, self.pub_topic, self.client_username, self.client_password)
 
-    def pub_function1(self, msg):
-        # msg_count = 1
-        # while msg_count < 10:
-            # time.sleep(1)
-        msg = f"{msg}"
+        # self.CONNECTION_STRING = 'mongodb+srv://mechatronics:BhamAomNunEarn@dimension.i10gagw.mongodb.net/'
+        # self.client = MongoClient(self.CONNECTION_STRING)
+        # self.Database = self.client['Dimension']
+        # self.Collection = self.Database['Captured']
+        # self.Collection2 = self.Database['Preprocessed_Image']
+
+        # self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+        # self.camera.Open()
+
+        # new_width = self.camera.Width.Value - self.camera.Width.Inc
+        # if new_width >= self.camera.Width.Min:
+        #     self.camera.Width.Value = new_width
+
+        # self.numberOfImagesToGrab = 1
+        # self.camera.StartGrabbingMax(self.numberOfImagesToGrab)
+
+    def pubpub(self, message):
+        msg = message
         result = self.mqtt_publisher.client.publish(self.pub_topic, msg)
-            # status = result.rc
-            # if status == 0:
-            #     print(f"Send `{msg}` to topic `{self.topic}`")
-            # else:
-            #     print(f"Failed to send message to topic {self.pub_topic}")
-            # msg_count += 1
 
     def on_message(self, client, userdata, msg):
         receive_msg = msg.payload.decode()
-        # print(f"Received message: {receive_msg} on topic: {msg.topic}")
-        
+        if receive_msg == 'isOn':
+            print('checkcam')
+            self.pubpub('isOn : Yes')
+        elif receive_msg == 'isTaken':
+            print('capture')
+            self.pubpub('isTaken : No')
+        elif receive_msg == 'isProcessed':
+            print('send to db')
+            self.pubpub('laskdsfdbjg')        
 
     def run_topic(self):
         print("Starting subscriber...")
         self.mqtt_subscriber.client.on_message = self.on_message
-        # self.mqtt_subscriber.client.loop_start()
-        time.sleep(2)  # Wait for the subscriber to establish connection
-        # print("Starting publisher...")
-        # self.pub_function1('Hello')
-        # self.mqtt_subscriber.client.loop_stop()
+        time.sleep(2)
+
 
     def run(self):
         self.mqtt_subscriber.client.loop_forever()
+
+    # def checkcamera(self):
+    #     if self.camera.IsGrabbing():
+    #         return True
+    #     else:
+    #         return False
+        
+    # def capture(self):
+    #     grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+    #     if grabResult.GrabSucceeded():
+    #         img = grabResult.Array
+    #         print(img.shape)
+    #     grabResult.Release()
+    #     return img
+
+    # def preprocessimage(self, image):
+    #     gray_image = image
+    #     mean = np.mean(gray_image)
+    #     median = cv2.medianBlur(gray_image, 3)
+    #     bin_image = cv2.threshold(median, 0.75*mean, 255, cv2.THRESH_BINARY)
+    #     self.publishtodb(bin_image[1], 1)
+    #     return bin_image[1]
+
+    # def publishtodb(self, image, collection):
+    #     if collection == 1:
+    #         collection = self.Collection
+    #     else:
+    #         collection = self.Collection2
+    #     image_binary = Binary(cv2.imencode('.jpg', image)[1].tobytes())
+    #     data = {
+    #         '_id': collection.count_documents({})+1,
+    #         'img_binary' : image_binary
+    #     }
+    #     self.Collection.insert_one(data)
+    #     return True
 
 
 if __name__ == '__main__':
---
+++ new_pylonnode2.py
@@ -0,0 +1,78 @@
+from pymongo import MongoClient
+import cv2
+from bson import Binary
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from pypylon import pylon
+
+class PylonAction():
+    def __init__(self):
+        self.CONNECTION_STRING = 'mongodb+srv://mechatronics:BhamAomNunEarn@dimension.i10gagw.mongodb.net/'
+        self.client = MongoClient(self.CONNECTION_STRING)
+        self.Database = self.client['Dimension']
+        self.Collection = self.Database['Captured']
+        self.Collection2 = self.Database['Preprocessed_Image']
+
+        self.camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+        self.camera.Open()
+
+        new_width = self.camera.Width.Value - self.camera.Width.Inc
+        if new_width >= self.camera.Width.Min:
+            self.camera.Width.Value = new_width
+
+        numberOfImagesToGrab = 1
+        self.camera.StartGrabbingMax(numberOfImagesToGrab)
+
+    def checkcamera(self):
+        if self.camera.IsGrabbing():
+            return True
+        else:
+            return False
+        
+    def capture(self):
+        grabResult = self.camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+        if grabResult.GrabSucceeded():
+            img = grabResult.Array
+            print(img.shape)
+        grabResult.Release()
+        return img
+
+    def preprocessimage(self, image):
+        gray_image = image
+        mean = np.mean(gray_image)
+        median = cv2.medianBlur(gray_image, 3)
+        bin_image = cv2.threshold(median, 0.75*mean, 255, cv2.THRESH_BINARY)
+        self.publishtodb(bin_image[1], 1)
+        return bin_image[1]
+
+    def publishtodb(self, image, collection):
+        if collection == 1:
+            collection = self.Collection
+        else:
+            collection = self.Collection2
+        image_binary = Binary(cv2.imencode('.jpg', image)[1].tobytes())
+        data = {
+            '_id': collection.count_documents({})+1,
+            'img_binary' : image_binary
+        }
+        self.Collection.insert_one(data)
+        return True
+
+if __name__ == '__main__':
+    action = PylonAction()
+    a = action.checkcamera()
+    if a:
+        captured_image = action.capture()
+        processed = action.preprocessimage(captured_image)
+        plt.imshow(processed)
+        plt.show()
+        b = action.publishtodb(processed, 2)
+
+# if checkcamera():
+#     while True:
+#         #if request
+#             img = capture()
+#             #if 2nd request
+#                 processed_image = preprocessimage(img)
+#                 publishtodb(processed_image)
